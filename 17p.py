ef278fcfa37d9c47b933599ac2da33040ebc0b8d
best-programmer-2021 <88088981+best-programmer-2021@users.noreply.github.com>
2021-07-30 13:59:42 +0430

    commiting from pc

---
+++ 17p.py
@@ -0,0 +1,128 @@
+import random
+my_random = ["a" , "b" , "c" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t" , "u" , "v" , "w" , "x" , "y" , "z" , "1" , "2" , "3" , "4" , "5" , "6" , "7" , "8" , "9" , "10" , "11" , "12" , "13" , "14" , "15" , "16" , "17" , "18" , "19" , "20" , "21" , "22" , "23" , "24" , "25" , "26" , "27" , "28" , "29" , "30" , "31" , "32" , "33" , "34" , "35" , "36"]
+my = random.choice(my_random)
+if my=="a" :
+    print("life doesnt get easier you just get stronger")
+elif my=="b":
+    print("If you want to achieve greatness stop asking for permission")    
+elif my=="c":
+    print("To live a creative life, we must lose our fear of being wrong")
+elif my=="d":
+    print("If you are not willing to risk the usual you will have to settle for the ordinary")
+elif my=="e":
+    print("All our dreams can come true if we have the courage to pursue them")
+elif my=="f":
+    print("he road to success and the road to failure are almost exactly the same")
+elif my=="g":
+    print("Good things come to people who wait, but better things come to those who go out and get them")   
+elif my=="h":
+    print("If you do what you always did, you will get what you always got")
+elif my=="i":
+    print("I have not failed. I’ve just found 10,000 ways that won’t work")
+elif my=="j":
+    print("Life is not about finding yourself. Life is about creating yourself")
+elif my=="k":
+    print("Don’t wait for extraordinary opportunities. Seize common occasions and make them great")    
+elif my=="l":
+    print("If you don’t design your own life plan, chances are you’ll fall into someone else’s plan And guess what they have planned for you? Not much")
+elif my=="m":
+    print("You must expect great things of yourself before you can do them")
+elif my=="n":
+    print("Don’t let yesterday take up too much of today")
+elif my=="o":
+    print("First say to yourself what you would be and then do what you have to do Epictetus")  
+elif my=="p":
+    print("You miss 100 of the shots you don’t take")   
+elif my=="q":
+    print("I am learning that who I am is not so bad")
+elif my=="r":
+    print("You learn more from failure than from success Don’t let it stop you Failure builds character")   
+elif my=="s":
+    print("You must be the change you wish to see in the world")
+elif my=="t":
+    print("For every minute you are angry you lose sixty seconds of happiness Ralph Waldo Emerson") 
+elif my=="u":
+    print("Too many of us are not living our dreams because we are living our fears")
+elif my=="v":
+    print("Live for ourselves not for showing that to others")
+elif my=="w":
+    print("A distant tour begins with one step")
+elif my=="x":
+    print("If you won’t be patient in the flow of river every driftwood will be a huge problem for you")  
+elif my=="y":
+    print("Who looses today won’t find tomorrow There is nothing important as today")  
+elif my=="z":
+    print("There is an American proverb that says If you face a problem try to find the solution not the reason")  
+elif my=="1":
+    print("Whatever human rise his trousers inset will be seen more")
+elif my=="2":
+    print("When an egg breaks by external power life ends When an egg breaks by internal power life begins General changes always begin with internal power")  
+elif my=="3":
+    print("Tips to be successful in life Appreciate those who love you Help those who need you Forgive those who hurt you Forget those who leave you")     
+elif my=="4":
+    print("Learn how to be happy with what you have while you purse all that you want")
+elif my=="5":
+    print("The only way to do great work is to love what you do")
+elif my=="6":
+    print("We can not change the direction of the wind  But  remember  that we can always adjust our sails")   
+elif my=="7":
+    print("Every struggle in your life has shaped you into the person  You are today  Be thankful for the hard times  they have made you STRONGER")   
+elif my=="8":
+    print("Life is a mirror and will reflect back to the thinker what he thinks into it")
+elif my=="9":
+    print("Change your thoughts and you change your world")
+elif my=="10":
+    print("Sometimes it is the smallest decisions that can change your life forever")
+elif my=="11":
+    print("Your life does not get better by chance  it goes better by change")
+elif my=="12":
+    print("When one door of happiness closes another opens but often we look so long at the closed door that we do not see the one that has been opened for us")    
+elif my=="13":
+    print("Your problem isn’t the problem Your reaction is the problem") 
+elif my=="14":
+    print("A successful man is one who can lay a firm foundation with the bricks others have thrown at him")   
+elif my=="15":
+    print("Success is to be measured not so much by the position that one has reached in life as by the obstacles which he has overcome")   
+elif my=="16":
+    print("Great minds discuss ideas  average minds discuss events small minds discuss people")    
+elif my=="17":
+    print("There are two types of people who will tell you that you cannot make a difference in this world those who are afraid to try and those who are afraid you will succeed")     
+elif my=="18":
+    print("The future belongs to those who believe in the beauty of their dreams")
+elif my=="19":
+    print("When you pray for others  God listens to you and blesses them  and sometimes  when you are safe and happy  remember that someone has prayed for you")   
+elif my=="20":
+    print("The way is not in the sky  The way is in the heart")  
+elif my=="21":
+    print("Be with ones who build you and your self  esteem Break up and move on from the ones who use you or bring you down")
+elif my=="22":
+    print("I find that the harder I work, the more luck I seem to have")
+elif my=="23":
+    print("Do not put the key to your happiness in someone else pocket keep it in your own")
+elif my=="24":
+    print("There are two ways to be happy  Either improve your reality  or lower your expectations") 
+elif my=="25":
+    print("When you learn teach when you get give")    
+elif my=="26":
+    print("You can you should and if you’re brave enough to start you will")    
+elif my=="27":
+    print("The cave you fear to enter holds the treasure you seek")  
+elif my=="28":
+    print("Success is not final failure is not fatal  it is the courage to continue that counts")  
+elif my=="29":
+    print("Only those that risk going too far can possibly find out how far one can go")   
+elif my=="30":
+    print("When the going gets tough, the tough get going")
+elif my=="31":
+    print("Most of the important things in the world have been accomplished by people who have kept on trying when there seemed to be no hope at all")
+elif my=="32":
+    print("Do not follow where the path may lead Go instead where there is no path and leave a trail")    
+elif my=="33":
+    print("Men are more mindful of wrongs than of benefits")
+elif my=="34":
+    print("In order to succeed your desire for success should be greater than your fear of failure")     
+elif my=="35":
+    print("Forgive your enemies but never forget their names")     
+elif my=="36":
+    print("Always forgive your enemies – nothing annoys them so much")      
+t = input("choose one key to exit : ")
